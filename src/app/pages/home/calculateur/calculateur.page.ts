825cd2e561208cf6f077040ec1a41d0a5c7c75bd
soumoud Mizen <soumoud.m@pixelstrade.com>
2020-07-27 10:53:02 +0100

    fix range issues

--- src/app/pages/home/calculateur/calculateur.page.ts
+++ src/app/pages/home/calculateur/calculateur.page.ts
@@ -10,28 +10,30 @@ export class CalculateurPage implements OnInit {
   @ViewChild('ranger') div: ElementRef;
   @ViewChild('input') input: ElementRef;
   @ViewChild('ticks') ticks: ElementRef;
- 
-  segment = 'hemoragie'
-  range = 12
+
+  segment = 'hemoragie';
+  range = 12;
   value = 200;
-  ranger
+  ranger;
   constructor() { }
 
   ngOnInit() {
 
   }
   ionViewDidEnter() {
-    let height = this.div.nativeElement.offsetHeight;
-    this.input.nativeElement.style.width = height - 40 + 'px'
-    this.ticks.nativeElement.style.width = height - 40 + 'px'
-    console.log("height ranger div ", height);
+    const height = this.div.nativeElement.offsetHeight;
+    const marginTopInput = (height / 2) - 16;const marginTopTicks = (height / 2) - 21;
+    this.input.nativeElement.style.width = height - 30 + 'px';
+    this.input.nativeElement.style.transform = `translate(-36%, ${marginTopInput}px) rotate(-90deg)`;
+    this.ticks.nativeElement.style.width = height - 30 + 'px';
+    this.ticks.nativeElement.style.transform = `translate(-44%, ${marginTopTicks}px) rotate(-90deg)`;
+    console.log('margin top ', marginTopInput + " " + marginTopTicks);
   }
   change() {
     if (this.between(this.range, 10, 30)){
-     
-      
+    
   }
-    if (this.between(this.range, 40,70 )){
+    if (this.between(this.range, 40, 70 )){
     
   }
 
